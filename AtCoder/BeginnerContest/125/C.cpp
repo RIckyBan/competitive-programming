60a4029744124223edd4b527ce757c90c4e23319
Riki Bando <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-03-03 15:17:32 +0900

    add 125-c

--- AtCoder/BeginnerContest/125/C.cpp
+++ AtCoder/BeginnerContest/125/C.cpp
@@ -20,54 +20,58 @@ set<int> st;
 int N, k, A[MAXN], ans = 0;
 bool flag;
 
-vi enum_div(int n)//nの約数を列挙
-{
-	vector<int> ret;
-	for(int i=1 ; i*i<=n ; ++i)
-	{
-		if(n%i == 0)
-		{
-			ret.push_back(i);
-			if(i*i!=n)
-			{
-				ret.push_back(n/i);
-			}
-		}
-	}
-	return ret;
-}
+// vi enum_div(int n)//nの約数を列挙
+// {
+// 	vector<int> ret;
+// 	for(int i=1 ; i*i<=n ; ++i)
+// 	{
+// 		if(n%i == 0)
+// 		{
+// 			ret.push_back(i);
+// 			if(i*i!=n)
+// 			{
+// 				ret.push_back(n/i);
+// 			}
+// 		}
+// 	}
+// 	return ret;
+// }
 
-void solve(){
-    vi div1 = enum_div(A[0]);
-    vi div2 = enum_div(A[1]);
+// void solve(){
+//     vi div1 = enum_div(A[0]);
+//     vi div2 = enum_div(A[1]);
 
-    for(auto i: div1){
-        st.insert(i);
-    }
+//     for(auto i: div1){
+//         st.insert(i);
+//     }
 
-    for(auto i: div2){
-        st.insert(i);
-    }
+//     for(auto i: div2){
+//         st.insert(i);
+//     }
 
-    for(auto i: st){
-        // cout << i << endl;
-        k = 0;
-        flag = true;
+//     for(auto i: st){
+//         // cout << i << endl;
+//         k = 0;
+//         flag = true;
 
-        rep(j, N){
-            if(A[j] % i != 0)
-                k++;
-            if(k > 1){
-                flag = false;
-                break;
-            }
-        }
-        if(flag)
-            ans = max(ans, i);
-    }
-    cout << ans << endl;
-}
+//         rep(j, N){
+//             if(A[j] % i != 0)
+//                 k++;
+//             if(k > 1){
+//                 flag = false;
+//                 break;
+//             }
+//         }
+//         if(flag)
+//             ans = max(ans, i);
+//     }
+//     cout << ans << endl;
+// }
 
+// 累積GCD
+
+
+// セグ木
 
 int main(){
     cin >> N;
