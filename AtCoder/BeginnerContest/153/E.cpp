a7bea72b4a7b95c45939e893180aa522ae0aaaa0
Riki Bando <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-03-10 16:11:38 +0900

    add abc153-E

--- AtCoder/BeginnerContest/153/E.cpp
+++ AtCoder/BeginnerContest/153/E.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+#define INF 1e9+5
 #define MAXN 1005
 #define MAXH 10005
 #define ll long long
@@ -14,9 +15,7 @@ using namespace std;
 #define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define pii pair<int, int>
 
-int H, N, A[MAXH], B[MAXH], id;
-
-ll dp[MAXN][MAXH], INF=1e9+5;
+int H, N, A[MAXH], B[MAXH], dp[MAXN][MAXH], id;
 
 void print_table(){
     rep(i, N+1){
@@ -28,17 +27,17 @@ void print_table(){
 }
 
 void solve(){
-    rep(i, N+1) rep(j, H+1) dp[i][j] = INF;
-    dp[0][0] = 0;
+    rep(i, N+1)rep(j, H+1) dp[i][j] = INF;
+    rep(i, N+1) dp[i][0] = 0;
 
     // print_table();
 
     // cout << endl;
 
-    rep(i, N){
+    for(int i = 1; i <= N; ++i){
         rep(j, H+1){
-            dp[i+1][j] = min(dp[i+1][j], dp[i][j]);    
-            dp[i+1][min(j+A[i], H)] = min(dp[i+1][j] + B[i], dp[i+1][min(j+A[i], H)]);
+            dp[i][j] = min(dp[i][j], dp[i-1][j]);
+            dp[i][min(j+A[i], H)] = min(dp[i][j]+B[i], dp[i][min(j+A[i], H)]); // 端の部分を何回も書き換えることに注意！！
         }
     }
 
@@ -47,16 +46,16 @@ void solve(){
     //     cout << dp[i][H] << endl;
 
     cout << dp[N][H] << endl;
-    }
+}
 
 int main(){
     cin >> H >> N;
+    A[0] = 0;
+    B[0] = 0;
 
     rep(i, N){
-        cin >> A[i] >> B[i];
+        cin >> A[i+1] >> B[i+1];
     }
 
-    // rep(i, N+1)
-    //     cout << A[i] << " " << B[i] << endl;
     solve();
 }
