2a69ebe1101c89e6f78d57a5e26b3c3dd36ca7ca
RIckyBan <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-05-07 22:29:26 +0900

    add abc139-D

---
+++ AtCoder/BeginnerContest/139/D.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 1e9
+#define MAXN 100005
+#define MAXM 100005
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define pii pair<int, int>
+
+ll ans = 0;
+ll N;
+
+void solve(){
+    ans = N*(N-1)/2;
+
+    cout << ans << endl;
+}
+
+int main(){
+    cin >> N;
+
+    solve();
+}
