caf9f3893b1d23f860e50c661fc0ce64e172c134
RickyBan <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-11-12 01:01:45 +0900

    add KEYENCE2019-C

--- AtCoder/KEYENCE/2019/C.cpp
+++ AtCoder/KEYENCE/2019/C.cpp
@@ -15,15 +15,61 @@ using namespace std;
 #define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define pii pair<int, int>
 
-ll ans;
-int A,B,C;
+ll ans, neg = 0;
+int N, num = 0;
+vi A,B,diff;
 
 void solve(){
+   if(num == 0){
+        cout << num << endl;
+        return;
+    }
 
+    ll tmp = 0;
+    rep(i, N){
+        tmp += diff[i];
+    }
+    if(tmp < 0){
+        cout << -1 << endl;
+        return;
+    }
+
+    sort(diff.begin(), diff.end(), greater<int>());
+
+    // for(auto v: diff){
+    //     cout << v << endl;
+    // }
+
+    int idx = 0;
+    while(neg < 0){
+        neg += diff[idx];
+        num++;
+        idx++;
+    }
+
+    cout << num << endl;
 }
 
 int main(){
-    cin >> A >> B >> C;
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    diff.resize(N);
+
+    rep(i, N){
+        cin >> A[i];
+    }
+    rep(i, N){
+        cin >> B[i];
+    }
+
+    rep(i, N){
+        diff[i] = A[i] - B[i];
+        if(diff[i] < 0){
+            neg += diff[i];
+            num++;
+        }
+    }
 
     solve();
 }
