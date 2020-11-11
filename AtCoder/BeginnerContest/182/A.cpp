c338b481ea169f3802a629e839b899f49fec4f28
Riki Bando <ricky1231@g.ecc.u-tokyo.ac.jp>
2020-11-11 23:56:17 +0900

    add ABC182-A

---
+++ AtCoder/BeginnerContest/182/A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 1e9
+#define MAXN 100005
+#define MAXM 100005
+#define MOD 1000000007
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define pii pair<int, int>
+
+ll ans;
+int A,B,C;
+
+void solve(){
+    cout << 2 * A + 100 - B << endl; 
+}
+
+int main(){
+    cin >> A >> B;
+
+    solve();
+}
