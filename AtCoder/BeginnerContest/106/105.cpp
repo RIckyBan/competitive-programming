4a5d0cdaeac523097a10fd88922cfa3b4ceccd88
RickyBan <rickyban1231@gmail.com>
2022-01-03 15:10:05 +0900

    update abc105-B

--- AtCoder/BeginnerContest/106/105.cpp
+++ AtCoder/BeginnerContest/106/105.cpp
@@ -6,26 +6,36 @@ using namespace std;
 
 int N;
 
-int division_count(int n){
+int division_count(int n)
+{
     int cnt = 0;
-    int range = sqrt(n); //整数に丸める！
 
-    for(int j = 1; j <= range; j++){
-        if(n % j == 0) cnt+=2;
+    for (int j = 1; j * j <= n; ++j)
+    {
+        if (n % j == 0)
+        {
+            cnt += 1;
+            if (j != n / j)
+            {
+                cnt += 1;
+            }
+        }
     }
-    if(n == pow(range,2)) cnt--;
 
     return cnt;
 }
 
-int solve(){
+int solve()
+{
     int ans = 0;
-    for(int i = 1; i <= N; i++)
-        if(i % 2 == 1 && division_count(i) == 8) ans++;
+    for (int i = 1; i <= N; i++)
+        if (i % 2 == 1 && division_count(i) == 8)
+            ans++;
     return ans;
 }
 
-int main(){
+int main()
+{
     cin >> N;
     // printf("%d\n",division_count(N));
     cout << solve() << endl;
