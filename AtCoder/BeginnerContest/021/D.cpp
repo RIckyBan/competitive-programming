6320b7440600a3b44334472374bcb7ad182dbcdc
RickyBan <rickyban1231@gmail.com>
2022-01-16 02:02:41 +0900

    add abc021-D

---
+++ AtCoder/BeginnerContest/021/D.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define INF 1e9
+#define MAXN 100005
+#define MAXM 100005
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define pii pair<int, int>
+
+ll ans = 0;
+ll N, K;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit()
+{
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++)
+    {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+void solve()
+{
+    COMinit();
+    cout << COM(N - 1 + K, K) << endl;
+}
+
+int main()
+{
+    cin >> N >> K;
+
+    solve();
+}
