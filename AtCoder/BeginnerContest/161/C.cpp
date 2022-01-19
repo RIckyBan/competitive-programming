ed83226a01797f48328386060a1129ff4cc06299
rickyban <rickyban1231@gmail.com>
2022-01-19 21:31:23 +0900

    add abc161-C

---
+++ AtCoder/BeginnerContest/161/C.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define INF 1e9
+#define MAXN 100005
+#define MAXM 100005
+#define MOD 1000000007
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define pii pair<int, int>
+
+ll ans = 0;
+ll N, K;
+
+void solve()
+{
+    while (N >= abs(N - K))
+    {
+        N = min(N % K, (K - N % K));
+    }
+    cout << N << endl;
+}
+
+int main()
+{
+    cin >> N >> K;
+
+    solve();
+}
