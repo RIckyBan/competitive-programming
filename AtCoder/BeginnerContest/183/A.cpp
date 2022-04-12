c6d1a9116c0c7fa2749d4b6db6955e2644102ba5
rickyban <rickyban1231@gmail.com>
2022-04-12 23:52:16 +0900

    add abc183-A

---
+++ AtCoder/BeginnerContest/183/A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define INF 1e9
+#define MAXN 100005
+#define MAXM 100005
+#define MOD 1000000007
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define pii pair<int, int>
+
+ll ans = 0;
+int X;
+
+int main()
+{
+    cin >> X;
+
+    cout << max(0, X) << endl;
+}
